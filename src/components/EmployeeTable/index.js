4702c673c78dad8f91a199b966aafae6b7621dc4
JohnDJake <john.d.jake@gmail.com>
2021-02-12 19:32:30 -0800

    sort by first or last name, ascending

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -9,14 +9,26 @@ export default class EmployeeTable extends Component {
         API.generate(100).then(({ data: { results } }) => this.setState({ employees: results }));
     }
 
+    sortByFirstNameAscending(a, b) {
+        if (a.name.first > b.name.first) return 1;
+        if (a.name.first < b.name.first) return -1;
+        return 0;
+    }
+
+    sortByLastNameAscending(a, b) {
+        if (a.name.last > b.name.last) return 1;
+        if (a.name.last < b.name.last) return -1;
+        return 0;
+    }
+
     render() {
         return (
             <table className="table">
                 <thead>
                     <tr>
                         <th scope="col">Picture</th>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
+                        <th scope="col" onClick={() => this.setState({sort: this.sortByFirstNameAscending})}>First Name</th>
+                        <th scope="col" onClick={() => this.setState({sort: this.sortByLastNameAscending})}>Last Name</th>
                         <th scope="col">Location</th>
                         <th scope="col">Email</th>
                         <th scope="col">Phone</th>
@@ -24,7 +36,7 @@ export default class EmployeeTable extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.employees.map(employee => <EmployeeRow key={employee.login.uuid} employee={employee} />)}
+                    {this.state.employees.sort(this.state.sort).map(employee => <EmployeeRow key={employee.login.uuid} employee={employee} />)}
 
                 </tbody>
             </table>
