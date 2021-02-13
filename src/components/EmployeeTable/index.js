26f24ad8ce616a8c4f723c2f85e16343c7f06ddc
JohnDJake <john.d.jake@gmail.com>
2021-02-12 19:32:48 -0800

    moved sort functions to an object

--- src/components/EmployeeTable/index.js
+++ src/components/EmployeeTable/index.js
@@ -2,23 +2,25 @@ import React, { Component } from 'react'
 import API from '../../utils/API';
 import EmployeeRow from '../EmployeeRow';
 
+const sorts = {
+    byFirstNameAscending: (a, b) => {
+        if (a.name.first > b.name.first) return 1;
+        if (a.name.first < b.name.first) return -1;
+        return 0;
+    },
+
+    byLastNameAscending: (a, b) => {
+        if (a.name.last > b.name.last) return 1;
+        if (a.name.last < b.name.last) return -1;
+        return 0;
+    }
+}
+
 export default class EmployeeTable extends Component {
     state = { employees: [] };
 
     componentDidMount() {
         API.generate(100).then(({ data: { results } }) => this.setState({ employees: results }));
-    }
-
-    sortByFirstNameAscending(a, b) {
-        if (a.name.first > b.name.first) return 1;
-        if (a.name.first < b.name.first) return -1;
-        return 0;
-    }
-
-    sortByLastNameAscending(a, b) {
-        if (a.name.last > b.name.last) return 1;
-        if (a.name.last < b.name.last) return -1;
-        return 0;
     }
 
     render() {
@@ -27,8 +29,8 @@ export default class EmployeeTable extends Component {
                 <thead>
                     <tr>
                         <th scope="col">Picture</th>
-                        <th scope="col" onClick={() => this.setState({sort: this.sortByFirstNameAscending})}>First Name</th>
-                        <th scope="col" onClick={() => this.setState({sort: this.sortByLastNameAscending})}>Last Name</th>
+                        <th scope="col" onClick={() => this.setState({ sort: "byFirstNameAscending" })}>First Name</th>
+                        <th scope="col" onClick={() => this.setState({ sort: "byLastNameAscending" })}>Last Name</th>
                         <th scope="col">Location</th>
                         <th scope="col">Email</th>
                         <th scope="col">Phone</th>
@@ -36,7 +38,7 @@ export default class EmployeeTable extends Component {
                     </tr>
                 </thead>
                 <tbody>
-                    {this.state.employees.sort(this.state.sort).map(employee => <EmployeeRow key={employee.login.uuid} employee={employee} />)}
+                    {this.state.employees.sort(sorts[this.state.sort]).map(employee => <EmployeeRow key={employee.login.uuid} employee={employee} />)}
 
                 </tbody>
             </table>
