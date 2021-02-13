027f991966fc5e10e237278108e5ade15fdd1223
JohnDJake <john.d.jake@gmail.com>
2021-02-12 16:24:34 -0800

    added navbar

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">Employee Directory</a>
+        </nav>
+    )
+}
