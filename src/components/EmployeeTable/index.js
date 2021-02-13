03176aa107a1be439d295883935f192bf4196175
JohnDJake <john.d.jake@gmail.com>
2021-02-12 18:30:43 -0800

    get generated employees from api and display in a table

---
+++ src/components/EmployeeTable/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import API from '../../utils/API';
+import EmployeeRow from '../EmployeeRow';
+
+export default class EmployeeTable extends Component {
+    state = { employees: [] };
+
+    componentDidMount() {
+        API.generate(100).then(({ data: { results } }) => this.setState({ employees: results }));
+    }
+
+    render() {
+        return (
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Picture</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Birthday</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {this.state.employees.map(employee => <EmployeeRow key={employee.id.value} employee={employee} />)}
+
+                </tbody>
+            </table>
+        )
+    }
+}
