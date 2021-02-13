32aea99aeddc75e0924e910be6b181991a4a6bc3
JohnDJake <john.d.jake@gmail.com>
2021-02-12 18:31:07 -0800

    display the employee table

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import EmployeeTable from './components/EmployeeTable'
 import Navbar from './components/Navbar'
 
 export default function App() {
     return (
         <div>
             <Navbar />
+            <div className="container">
+                <EmployeeTable />
+            </div>
         </div>
     );
 }
